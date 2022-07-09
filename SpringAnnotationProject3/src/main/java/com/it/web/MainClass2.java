e1003542c526d1cc29ba18e91e868bc9d2189905
Administrator <Administrator@804-t>
2022-07-09 17:51:01 +0900

    1111

--- SpringAnnotationProject3/src/main/java/com/it/web/MainClass2.java
+++ SpringAnnotationProject3/src/main/java/com/it/web/MainClass2.java
@@ -1,10 +1,31 @@
 package com.it.web;
 
+
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.it.config.EmpConfig;
+import com.it.dao.*;
+import java.util.*;
+// 최근 => 스프링 (대부분 XML없이 => Java)
 public class MainClass2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+        AnnotationConfigApplicationContext app=
+        		new AnnotationConfigApplicationContext(EmpConfig.class);
+        EmpDAO dao=app.getBean("empDAO",EmpDAO.class);
+        List<EmpVO> list=dao.empJoinData();
+        for(EmpVO vo:list)
+        {
+        	System.out.println(vo.getEmpno()+" "
+        			+vo.getEname()+" "
+        			+vo.getJob()+" "
+        			+vo.getDbday()+" "
+        			+vo.getSal()+" "
+        			+vo.getDvo().getDname()+" "
+        			+vo.getDvo().getLoc());
+        }
 	}
 
 }
