4f2bdcca13ecb0e696e95ca48e7d21430bceda7f
Administrator <Administrator@804-t>
2022-07-09 15:27:59 +0900

    1111

--- SpringAnnotationProject/src/main/java/com/it/dao/EmpDeptDAO.java
+++ SpringAnnotationProject/src/main/java/com/it/dao/EmpDeptDAO.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import java.util.*;
 // 기능 => 데이터베이스 연결 (스프링 컨테이너 관리) => 메모리 할당 요청 
-@Repository
+@Repository // 자동 ID 생성 ==> 클래스명 (첫자만 소문자)
 public class EmpDeptDAO {
    // 구현된 클래스의 주소를 자동으로 얻어 온다 (자동 주입 요청) => 수동 (getBean())
    @Autowired
--- SpringAnnotationProject/src/main/java/com/it/web/MainClass.java
+++ SpringAnnotationProject/src/main/java/com/it/web/MainClass.java
@@ -1,10 +1,30 @@
 package com.it.web;
+import java.util.*;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.it.dao.*;
 public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+        ApplicationContext app=
+        		new ClassPathXmlApplicationContext("app.xml");
+        EmpDeptDAO dao=(EmpDeptDAO)app.getBean("empDeptDAO");
+        List<EmpVO> list=dao.empListData();
+        for(EmpVO vo:list)
+        {
+        	System.out.println(vo.getEname()+" "+vo.getJob()+" "
+        			+vo.getHiredate());
+        }
+        System.out.println("===============================");
+        List<DeptVO> list2=dao.deptListData();
+        for(DeptVO vo:list2)
+        {
+        	System.out.println(vo.getDeptno()+" "+vo.getDname()+" "+vo.getLoc());
+        }
+        
 	}
 
 }
