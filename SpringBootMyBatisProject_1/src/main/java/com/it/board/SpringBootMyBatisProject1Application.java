b1fe8f934706fa20f1a19842887fd51a9d8a0060
Administrator <Administrator@804-t>
2022-07-30 10:20:21 +0900

    1111

---
+++ SpringBootMyBatisProject_1/src/main/java/com/it/board/SpringBootMyBatisProject1Application.java
@@ -0,0 +1,13 @@
+package com.it.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMyBatisProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMyBatisProject1Application.class, args);
+	}
+
+}
---
+++ SpringBootMyBatisProject_1/src/test/java/com/it/board/SpringBootMyBatisProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.it.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMyBatisProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
