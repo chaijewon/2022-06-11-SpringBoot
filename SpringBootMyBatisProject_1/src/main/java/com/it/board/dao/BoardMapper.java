80d6c8c6c2aa5b8333bdae7ea7bfd0389fdbbfc5
Administrator <Administrator@804-t>
2022-07-30 10:48:31 +0900

    1111

---
+++ SpringBootMyBatisProject_1/src/main/java/com/it/board/dao/BoardMapper.java
@@ -0,0 +1,16 @@
+package com.it.board.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+import java.util.*;
+import com.it.board.vo.*;
+@Repository
+@Mapper
+public interface BoardMapper {
+  /*
+   *   <select id="boardListData" resultType="com.it.board.vo.BoardVO" parameterType="int">
+   *               -------------              -----------------------                -----
+   *               메소드명                                              리턴형                                           매개변수
+   */
+  public List<BoardVO> boardListData(int start); 
+}
--- SpringBootMyBatisProject_1/src/main/java/com/it/board/vo/BoardVO.java
+++ SpringBootMyBatisProject_1/src/main/java/com/it/board/vo/BoardVO.java
@@ -5,8 +5,16 @@ import lombok.Getter;
 import lombok.Setter;
 @Setter
 @Getter
+// MySql에서 데이터를 모아서 저장 
+/*
+ *    1. 스프링 프레임워크 => XML설정이 많이 존재 
+ *    2. XML을 사용하지 않고 스프링 프레임워크 다루기 ==> 스프링 부트
+ *    3. 스프링을 더 쉽게 이용이 가능하게 하는 도구 (스프링 부트)
+ *       XML => application.properties을 이용해서 필요한 정보를 전송 
+ *    4. 최근 => STS (프레임워크가 제거) => STS4 (부트)
+ */
 public class BoardVO {
    private int no,hit;
-   private String name,subject,content,pwd;
+   private String name,subject,content,pwd,dbday;
    private Date regdate;
 }
